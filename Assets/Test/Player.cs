b8d5bbec67723b57bb9652acaf39772af6b2469a
BlueBug <iopzxz@gmail.com>
2017-03-06 12:49:58 -0500

    11
    1111

---
+++ Assets/Test/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Player : NetworkBehaviour {
+    public GameObject m_mainCamera;
+   
+    public override void OnStartAuthority()
+    {
+        base.OnStartAuthority();
+    }
+    public override void OnStartLocalPlayer()
+    {
+        base.OnStartLocalPlayer();
+        m_mainCamera.SetActive(true);
+    }
+    void Update()
+    {
+        if (!isLocalPlayer) return;
+    }
+}
